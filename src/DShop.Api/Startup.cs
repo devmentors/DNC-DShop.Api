dad5119869c7c481654cfaeea7e4176f41f6dc7f
GooRiOn <foreverframenet@gmail.com>
2018-02-11 21:25:26 +0100

    Added default ASP.NET Core project

--- src/DShop.Api/Startup.cs
+++ src/DShop.Api/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace DShopApi
+namespace DShop.Api
 {
     public class Startup
     {
