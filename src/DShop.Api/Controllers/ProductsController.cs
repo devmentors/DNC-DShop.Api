e45ac6df90b97f417d239364e6400fd928b2e2b7
GooRiOn <foreverframenet@gmail.com>
2018-02-14 19:59:17 +0100

    Added service forwarders with temp hard coded URI (will wrap into factory with dynamic Docker DNS)

--- src/DShop.Api/Controllers/ProductsController.cs
+++ src/DShop.Api/Controllers/ProductsController.cs
@@ -1,6 +1,11 @@
-﻿using DShop.Common.Bus;
+﻿using DShop.Api.ServiceForwarders;
+using DShop.Common.Bus;
 using DShop.Messages.Commands.Products;
+using DShop.Messages.ReadModels;
 using Microsoft.AspNetCore.Mvc;
+using RestEase;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DShop.Api.Controllers
@@ -8,15 +13,37 @@ namespace DShop.Api.Controllers
     [Route("api/[controller]")]
     public class ProductsController : Controller
     {
+        private readonly IProductsApi _productsApi;
         private readonly IPublishBus _publishBus;
 
         public ProductsController(IPublishBus publishBus)
         {
+            _productsApi = RestClient.For<IProductsApi>("http://localhost:5001/"); // will be Docker internal DNS
             _publishBus = publishBus;
         }
 
-        [HttpPut("")]
+        [HttpGet("{id}/Details")]
+        public async Task<ProductDetailsReadModel> GetProductDetailsAsync(Guid id)
+            => await _productsApi.GetProductDetailsAsync(id);
+
+        [HttpGet("All")]
+        public async Task<IEnumerable<ProductReadModel>> GetAllProductsAsync()
+            => await _productsApi.GetAllProductsAsync();
+
+        [HttpGet("Filtered")]
+        public async Task<IEnumerable<ProductReadModel>> GetProductsByVendorAsync([FromQuery] string vendor)
+            => await _productsApi.GetProductsByVendorAsync(vendor);
+
+        [HttpPut]
         public async Task CreateAsync([FromBody] CreateProduct command)
+            => await _publishBus.PublishCommandAsync(command, null);
+
+        [HttpPost]
+        public async Task UpdateAsync([FromBody] UpdateProduct command)
+            => await _publishBus.PublishCommandAsync(command, null);
+
+        [HttpDelete]
+        public async Task DeleteAsync([FromBody] DeleteProduct command)
             => await _publishBus.PublishCommandAsync(command, null);
     }
 }
---
+++ src/DShop.Api/ServiceForwarders/IProductsApi.cs
@@ -0,0 +1,20 @@
+﻿using DShop.Messages.ReadModels;
+using RestEase;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DShop.Api.ServiceForwarders
+{
+    public interface IProductsApi
+    {
+        [Get("api/Products/{id}/Details")]
+        Task<ProductDetailsReadModel> GetProductDetailsAsync([Path] Guid id);
+
+        [Get("api/Products/All")]
+        Task<IEnumerable<ProductReadModel>> GetAllProductsAsync();
+
+        [Get("api/Products/Filtered")]
+        Task<IEnumerable<ProductReadModel>> GetProductsByVendorAsync(string vendor);
+    }
+}
