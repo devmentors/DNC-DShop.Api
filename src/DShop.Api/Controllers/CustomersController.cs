8d9aba6794fa4b98d417eb59305c6cd6f3e5cbe5
Piotr Gankiewicz <spetz90@gmail.com>
2018-02-20 15:12:48 +0100

    Customers controller, sln

---
+++ src/DShop.Api/Controllers/CustomersController.cs
@@ -0,0 +1,19 @@
+﻿using DShop.Api.ServiceForwarders;
+using DShop.Common.RabbitMq;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace DShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomersController : Controller
+    {
+        private readonly IBusPublisher _busPublisher;
+
+        public CustomersController(IBusPublisher busPublisher)
+        {
+            _busPublisher = busPublisher;
+        }
+    }
+}
