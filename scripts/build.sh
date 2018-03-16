a0dfdf598e449f2b3e78c4563e625ffa847d8809
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2018-03-16 08:46:32 +0100

    Build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-dotnet build
+MYGET_ENV=""
+case "$TRAVIS_BRANCH" in
+  "develop")
+    MYGET_ENV="-dev"
+    ;;
+esac
+
+dotnet build -c Release --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/dnc-dshop$MYGET_ENV/api/v3/index.json" --no-cache
