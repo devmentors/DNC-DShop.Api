c69a81f736387b4ea8649d4f515a3e10c332f4bf
GooRiOn <foreverframenet@gmail.com>
2018-03-18 17:32:39 +0100

    Removed AllowAnonymous attributes inside ProductsController

--- src/DShop.Api/Controllers/ProductsController.cs
+++ src/DShop.Api/Controllers/ProductsController.cs
@@ -21,12 +21,10 @@ namespace DShop.Api.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public async Task<IActionResult> BrowseAsync([FromQuery] BrowseProducts query)
             => GetAsync(await _storage.BrowseAsync(query));
 
         [HttpGet("{id}")]
-        [AllowAnonymous]
         public async Task<IActionResult> GetAsync(Guid id)
             => GetAsync(await _storage.GetAsync(id));
 
