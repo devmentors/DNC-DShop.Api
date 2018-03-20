cc355dc854287c82217413d3b94bbeb554787716
Piotr Gankiewicz <spetz90@gmail.com>
2018-03-20 14:03:35 +0100

    Updated build scripts

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-dotnet publish --no-restore ./src/DShop.Api -c Release -o ./bin/Docker
-
 DOCKER_ENV=''
 DOCKER_TAG=''
 
---
+++ scripts/dotnet-publish.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet publish --no-restore ./src/DShop.Api -c Release -o ./bin/Docker
