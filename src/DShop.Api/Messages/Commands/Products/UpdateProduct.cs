3991291b782bffef68eb276b1399b871b501f00e
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2018-11-18 21:52:54 +0100

    Product messages fix

--- src/DShop.Api/Messages/Commands/Products/UpdateProduct.cs
+++ src/DShop.Api/Messages/Commands/Products/UpdateProduct.cs
@@ -10,16 +10,21 @@ namespace DShop.Api.Messages.Commands.Products
         public Guid Id { get; }
         public string Name { get; }
         public string Description { get; }
+        public string Vendor { get; }
         public decimal Price { get; }
+        public int Quantity { get; }
 
         [JsonConstructor]
         public UpdateProduct(Guid id, string name,
-            string description, decimal price)
+            string description, string vendor, 
+            decimal price, int quantity)
         {
             Id = id;
             Name = name;
             Description = description;
+            Vendor = vendor;
             Price = price;
+            Quantity = quantity;
         }
     }
 }
